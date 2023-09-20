7983c59cb955a88f0cd5914f1067d35c91c1d1a4
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-09-20 19:58:27 +0100

    removed protect layout in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom"
 import { AuthLayout } from "./components/layouts/auth"
-import Protected from "./utils/protected"
 
 export const App = () => {
     return (
         <>
-            <Protected path="/app">
-                <AuthLayout>
-                    <Outlet />
-                </AuthLayout>
-            </Protected>
+            <AuthLayout>
+                <Outlet />
+            </AuthLayout>
         </>
     )
 }
